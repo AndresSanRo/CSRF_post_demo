618151ce74bd7bc900dfbf07b55ba5226dcbe00c
Andrés Sánchez Robleño <andressanchezro@hotmail.com>
2019-01-28 14:05:45 +0100

    Added "VulnerableController"

---
+++ CSRF_post_demo/Controllers/VulnerableController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSRF_post_demo.Controllers
+{
+    public class VulnerableController : Controller
+    {
+        // GET: Vulnerable
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
