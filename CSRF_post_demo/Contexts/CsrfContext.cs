ae5d6b762c81675ec5d794eb89a17d6a5e604ddc
Andrés Sánchez Robleño <andressanchezro@hotmail.com>
2019-01-28 14:03:20 +0100

    Added entity context

---
+++ CSRF_post_demo/Contexts/CsrfContext.cs
@@ -0,0 +1,15 @@
+﻿using CSRF_post_demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CSRF_post_demo.Contexts
+{
+    public class CsrfContext : DbContext
+    {
+        public CsrfContext() : base("name=tjConnStr") { }
+        public DbSet<User> Users { get; set; }
+    }
+}
