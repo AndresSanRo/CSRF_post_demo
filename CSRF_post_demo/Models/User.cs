a566a5705c0a105b6223c5b1e0d469ef6443f500
Andrés Sánchez Robleño <andressanchezro@hotmail.com>
2019-01-28 14:03:14 +0100

    Added model to map DDBB table

---
+++ CSRF_post_demo/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CSRF_post_demo.Models
+{
+    [Table("CSRFUSER")]
+    public class User
+    {
+        [Key]
+        [Column("CSRF_ID")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Column("CSRF_USERNAME")]
+        public string Name { get; set; }
+        [Column("CSRF_USERPASSWORD")]
+        public string Password { get; set; }
+    }
+}
