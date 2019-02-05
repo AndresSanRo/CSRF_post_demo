c8dde0624bbba072141f35621853bccf20598bdb
Andrés <andressanchezro@hotmail.com>
2019-02-05 19:34:52 +0100

    Config Hacker project

--- Hacker/Controllers/HomeController.cs
+++ Hacker/Controllers/HomeController.cs
@@ -7,20 +7,8 @@ using System.Web.Mvc;
 namespace Hacker.Controllers
 {
     public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
+    {      
+        public ActionResult Hacker()
         {
             ViewBag.Message = "Your contact page.";
 
