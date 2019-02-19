c26f1491c72b52cd675975b9280a2662247b94cc
Andrés <andressanchezro@hotmail.com>
2019-02-19 19:38:35 +0100

    change passwords

--- CSRF_post_demo/Contexts/CsrfContext.cs
+++ CSRF_post_demo/Contexts/CsrfContext.cs
@@ -10,7 +10,7 @@ namespace CSRF_post_demo.Contexts
     public class CsrfContext : DbContext
     {
         //Context of the database
-        public CsrfContext() : base("name=tjConnStr") { }
+        public CsrfContext() : base("name=connStr") { }
         //DbSet to retrieve users
         public DbSet<User> Users { get; set; }
     }
